55aff80dd1644eaeab08af39baa390f6cf3151c6
Egor Tsyganchuk <cyaneor@gmail.com>
2025-02-18 23:01:45 +0300

    Добавлены тесты для проверки функции копирования строки

--- src/str_raw.cpp
+++ src/str_raw.cpp
@@ -2,74 +2,130 @@
 #include <ae/str_raw.h>
 
 TEST(ae_str_raw_len_with, returns_correct_index_for_null_terminator) {
-    const ae_char_t *test_str = "Hello";
-    ae_usize_t result = ae_str_raw_len_with(test_str, 6);
-    EXPECT_EQ(result, 5);
+  const ae_char_t *test_str = "Hello";
+  ae_usize_t result = ae_str_raw_len_with(test_str, 6);
+  EXPECT_EQ(result, 5);
 }
 
-TEST(ae_str_raw_len_with, returns_correct_index_for_null_terminator_mid_string) {
-    const ae_char_t *test_str = "Hell\0o";
-    ae_usize_t result = ae_str_raw_len_with(test_str, 6);
-    EXPECT_EQ(result, 4);
+TEST(ae_str_raw_len_with,
+     returns_correct_index_for_null_terminator_mid_string) {
+  const ae_char_t *test_str = "Hell\0o";
+  ae_usize_t result = ae_str_raw_len_with(test_str, 6);
+  EXPECT_EQ(result, 4);
 }
 
 TEST(ae_str_raw_len_with, handles_empty_string) {
-    const ae_char_t *test_str = "";
-    ae_usize_t result = ae_str_raw_len_with(test_str, 0);
-    EXPECT_EQ(result, 0);
+  const ae_char_t *test_str = "";
+  ae_usize_t result = ae_str_raw_len_with(test_str, 0);
+  EXPECT_EQ(result, 0);
 }
 
 TEST(ae_str_raw_len_with, returns_full_length_if_no_null_terminator) {
-    const ae_char_t *test_str = "Hello";
-    ae_usize_t result = ae_str_raw_len_with(test_str, 6);
-    EXPECT_EQ(result, 5);
+  const ae_char_t *test_str = "Hello";
+  ae_usize_t result = ae_str_raw_len_with(test_str, 6);
+  EXPECT_EQ(result, 5);
 }
 
 TEST(ae_str_raw_len_with, handles_null_terminator_at_end) {
-    const ae_char_t *test_str = "Test\0";
-    ae_usize_t result = ae_str_raw_len_with(test_str, 5);
-    EXPECT_EQ(result, 4);
+  const ae_char_t *test_str = "Test\0";
+  ae_usize_t result = ae_str_raw_len_with(test_str, 5);
+  EXPECT_EQ(result, 4);
 }
 
 TEST(ae_str_raw_len_with, returns_length_for_shorter_string) {
-    const ae_char_t *test_str = "Short";
-    ae_usize_t result = ae_str_raw_len_with(test_str, 3);
-    EXPECT_EQ(result, 0);
+  const ae_char_t *test_str = "Short";
+  ae_usize_t result = ae_str_raw_len_with(test_str, 3);
+  EXPECT_EQ(result, 0);
 }
 
 TEST(ae_str_raw_len_with, correctly_handles_null_input) {
-    const ae_char_t *test_str = nullptr;
-    ae_usize_t result = ae_str_raw_len_with(test_str, 0);
-    EXPECT_EQ(result, 0);
+  const ae_char_t *test_str = nullptr;
+  ae_usize_t result = ae_str_raw_len_with(test_str, 0);
+  EXPECT_EQ(result, 0);
 }
 
 TEST(ae_str_raw_len_with, handles_single_character_string) {
-    const ae_char_t *test_str = "A";
-    ae_usize_t result = ae_str_raw_len_with(test_str, 1);
-    EXPECT_EQ(result, 0);
+  const ae_char_t *test_str = "A";
+  ae_usize_t result = ae_str_raw_len_with(test_str, 1);
+  EXPECT_EQ(result, 0);
 }
 
 TEST(ae_str_raw_len, normal) {
-    const ae_char_t str[] = "Hello, world!";
-    EXPECT_EQ(ae_str_raw_len(str), 13);
+  const ae_char_t str[] = "Hello, world!";
+  EXPECT_EQ(ae_str_raw_len(str), 13);
 }
 
 TEST(ae_str_raw_len, empty_string) {
-    const ae_char_t str[] = "";
-    EXPECT_EQ(ae_str_raw_len(str), 0);
+  const ae_char_t str[] = "";
+  EXPECT_EQ(ae_str_raw_len(str), 0);
 }
 
 TEST(ae_str_raw_len, null_pointer) {
-    EXPECT_EQ(ae_str_raw_len(nullptr), 0);
+  EXPECT_EQ(ae_str_raw_len(nullptr), 0);
 }
 
 TEST(ae_str_raw_len, single_character) {
-    const ae_char_t str[] = "A";
-    EXPECT_EQ(ae_str_raw_len(str), 1);
+  const ae_char_t str[] = "A";
+  EXPECT_EQ(ae_str_raw_len(str), 1);
 }
 
 TEST(ae_str_raw_len, long_string) {
-    const ae_char_t str[] =
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-    EXPECT_EQ(ae_str_raw_len(str), 56);
+  const ae_char_t str[] =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+  EXPECT_EQ(ae_str_raw_len(str), 56);
+}
+
+TEST(ae_str_raw_copy_with, copy_normal_string) {
+  const char *src = "Hello, World!";
+  size_t len = strlen(src);
+
+  ae_char_t dest[len + 1];
+  memset(dest, 0, sizeof(dest));
+
+  ae_str_raw_copy_with(dest, src, len);
+  EXPECT_STREQ(dest, "Hello, World!");
+}
+
+TEST(ae_str_raw_copy_with, copy_empty_string) {
+  const char *src = "";
+  size_t len = strlen(src);
+
+  ae_char_t dest[len + 1];
+  memset(dest, 0, sizeof(dest));
+
+  ae_str_raw_copy_with(dest, src, len);
+  EXPECT_STREQ(dest, "");
+}
+
+TEST(ae_str_raw_copy_with, copy_string_with_null_character) {
+  const char *src = "Hello\0World";
+  size_t len = strlen(src);
+
+  ae_char_t dest[len + 1];
+  memset(dest, 0, sizeof(dest));
+
+  ae_str_raw_copy_with(dest, src, len);
+  EXPECT_STREQ(dest, "Hello");
+}
+
+TEST(ae_str_raw_copy_with, copy_exact_length_string) {
+  const char *src = "ExactLengthTest";
+  size_t len = strlen(src);
+
+  ae_char_t dest[len + 1];
+  memset(dest, 0, sizeof(dest));
+
+  ae_str_raw_copy_with(dest, src, len);
+  EXPECT_STREQ(dest, "ExactLengthTest");
+}
+
+TEST(ae_str_raw_copy_with, empty_copy) {
+  const char *src = "";
+  size_t len = 0;
+
+  ae_char_t dest[1];
+  memset(dest, 0, sizeof(dest));
+
+  ae_str_raw_copy_with(dest, src, len);
+  EXPECT_EQ(dest[0], '\0');
 }
