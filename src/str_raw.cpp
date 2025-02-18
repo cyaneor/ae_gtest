24eb4da8c7d0ae422d23b345a2e01662f60f91a6
Egor Tsyganchuk <cyaneor@gmail.com>
2025-02-18 23:18:20 +0300

    Добавлены тесты для проверки функции копирования строки с размером

--- src/str_raw.cpp
+++ src/str_raw.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
+
 #include <ae/str_raw.h>
+#include <ae/runtime_error.h>
+#include <ae/runtime_error_code.h>
 
 TEST(ae_str_raw_len_with, returns_correct_index_for_null_terminator) {
   const ae_char_t *test_str = "Hello";
@@ -122,10 +125,69 @@ TEST(ae_str_raw_copy_with, copy_exact_length_string) {
 TEST(ae_str_raw_copy_with, empty_copy) {
   const char *src = "";
   size_t len = 0;
-
-  ae_char_t dest[1];
-  memset(dest, 0, sizeof(dest));
+  ae_char_t dest[1] = {};
 
   ae_str_raw_copy_with(dest, src, len);
   EXPECT_EQ(dest[0], '\0');
+}
+
+TEST(ae_str_raw_copy, copy_normal_string) {
+  const char *src = "Hello, World!";
+  size_t len = strlen(src);
+
+  ae_char_t dest[len + 1];
+  memset(dest, 0, sizeof(dest));
+
+  ae_str_raw_copy(dest, src);
+  EXPECT_STREQ(dest, "Hello, World!");
+}
+
+TEST(ae_str_raw_copy, copy_empty_string) {
+  const char *src = "";
+  size_t len = strlen(src);
+
+  ae_char_t dest[len + 1];
+  memset(dest, 0, sizeof(dest));
+
+  ae_str_raw_copy(dest, src);
+  EXPECT_STREQ(dest, "");
+}
+
+TEST(ae_str_raw_copy, copy_string_with_null_character) {
+  const char *src = "Hello\0World";
+  size_t len = strlen(src);
+
+  ae_char_t dest[len + 1];
+  memset(dest, 0, sizeof(dest));
+
+  ae_str_raw_copy(dest, src);
+  EXPECT_STREQ(dest, "Hello");
+}
+
+TEST(ae_str_raw_copy, copy_exact_length_string) {
+  const char *src = "ExactLengthTest";
+  size_t len = strlen(src);
+
+  ae_char_t dest[len + 1];
+  memset(dest, 0, sizeof(dest));
+
+  ae_str_raw_copy(dest, src);
+  EXPECT_STREQ(dest, "ExactLengthTest");
+}
+
+TEST(ae_str_raw_copy, empty_string) {
+  const char *src = "";
+  ae_char_t dest[1] = {};
+
+  ae_str_raw_copy(dest, src);
+  EXPECT_EQ(dest[0], '\0');
+}
+
+TEST(ae_str_raw_copy, null_pointer) {
+  ae_char_t *dest = nullptr;
+  const char *src = "SomeString";
+
+  ae_str_raw_copy(dest, src);
+  EXPECT_EQ(ae_error_get_code_and_clear(ae_runtime_error()),
+            AE_RUNTIME_ERROR_NULL_POINTER);
 }
