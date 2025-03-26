7ba66674d4e83c7f2206b24495dd96b59ca56ba5
Egor Tsyganchuk <cyaneor@gmail.com>
2025-03-26 22:31:12 +0300

    Добавлены тесты

--- src/str_raw.cpp
+++ src/str_raw.cpp
@@ -5,7 +5,7 @@ TEST(ae_str_raw_find_char, find_existing_char_in_middle) {
   const char *str = "Hello, world!";
   ae_usize_t len = strlen(str);
   ae_char_t value = 'w';
-  const ae_str_raw_t result = ae_str_raw_find_char(str, len, value);
+  const ae_char_t * result = ae_str_raw_find_char(str, len, value);
   EXPECT_EQ(result, str + 7);
   EXPECT_EQ(*result, 'w');
 }
@@ -14,7 +14,7 @@ TEST(ae_str_raw_find_char, find_first_char) {
   const char *str = "Hello, world!";
   ae_usize_t len = strlen(str);
   ae_char_t value = 'H';
-  const ae_str_raw_t result = ae_str_raw_find_char(str, len, value);
+  const ae_char_t * result = ae_str_raw_find_char(str, len, value);
   EXPECT_EQ(result, str);
   EXPECT_EQ(*result, 'H');
 }
@@ -23,7 +23,7 @@ TEST(ae_str_raw_find_char, find_last_char) {
   const char *str = "Hello, world!";
   ae_usize_t len = strlen(str);
   ae_char_t value = '!';
-  const ae_str_raw_t result = ae_str_raw_find_char(str, len, value);
+  const ae_char_t * result = ae_str_raw_find_char(str, len, value);
   EXPECT_EQ(result, str + len - 1);
   EXPECT_EQ(*result, '!');
 }
@@ -32,7 +32,7 @@ TEST(ae_str_raw_find_char, char_not_found) {
   const char *str = "Hello, world!";
   ae_usize_t len = strlen(str);
   ae_char_t value = 'z';
-  const ae_str_raw_t result = ae_str_raw_find_char(str, len, value);
+  const ae_char_t * result = ae_str_raw_find_char(str, len, value);
   EXPECT_EQ(result, nullptr);
 }
 
@@ -40,7 +40,7 @@ TEST(ae_str_raw_find_char, empty_string) {
   const char *str = "";
   ae_usize_t len = 0;
   ae_char_t value = 'a';
-  const ae_str_raw_t result = ae_str_raw_find_char(str, len, value);
+  const ae_char_t * result = ae_str_raw_find_char(str, len, value);
   EXPECT_EQ(result, nullptr);
 }
 
@@ -48,7 +48,7 @@ TEST(ae_str_raw_find_char, single_char_string_found) {
   const char *str = "x";
   ae_usize_t len = 1;
   ae_char_t value = 'x';
-  const ae_str_raw_t result = ae_str_raw_find_char(str, len, value);
+  const ae_char_t * result = ae_str_raw_find_char(str, len, value);
   EXPECT_EQ(result, str);
   EXPECT_EQ(*result, 'x');
 }
@@ -57,34 +57,34 @@ TEST(ae_str_raw_find_char, single_char_string_not_found) {
   const char *str = "x";
   ae_usize_t len = 1;
   ae_char_t value = 'y';
-  const ae_str_raw_t result = ae_str_raw_find_char(str, len, value);
+  const ae_char_t * result = ae_str_raw_find_char(str, len, value);
   EXPECT_EQ(result, nullptr);
 }
 
 TEST(ae_str_raw_find_of_null_terminator, non_empty_string) {
   const char *str = "Hello, world!";
-  const ae_str_raw_t result = ae_str_raw_find_of_null_terminator(str);
+  const ae_char_t * result = ae_str_raw_find_of_null_terminator(str);
   EXPECT_EQ(result, str + strlen(str));
   EXPECT_EQ(*result, '\0');
 }
 
 TEST(ae_str_raw_find_of_null_terminator, empty_string) {
   const char *str = "";
-  const ae_str_raw_t result = ae_str_raw_find_of_null_terminator(str);
+  const ae_char_t * result = ae_str_raw_find_of_null_terminator(str);
   EXPECT_EQ(result, str);
   EXPECT_EQ(*result, '\0');
 }
 
 TEST(ae_str_raw_find_of_null_terminator, single_char_string) {
   const char *str = "x";
-  const ae_str_raw_t result = ae_str_raw_find_of_null_terminator(str);
+  const ae_char_t * result = ae_str_raw_find_of_null_terminator(str);
   EXPECT_EQ(result, str + 1);
   EXPECT_EQ(*result, '\0');
 }
 
 TEST(ae_str_raw_find_of_null_terminator, multiple_nulls) {
   const char str[] = {'H', 'i', '\0', '!', '\0'};
-  const ae_str_raw_t result = ae_str_raw_find_of_null_terminator(str);
+  const ae_char_t * result = ae_str_raw_find_of_null_terminator(str);
   EXPECT_EQ(result, str + 2);
   EXPECT_EQ(*result, '\0');
 }
@@ -112,4 +112,126 @@ TEST(ae_str_raw_len, multiple_nulls) {
   const char str[] = {'H', 'i', '\0', '!', '\0'};
   ae_usize_t result = ae_str_raw_len(str);
   EXPECT_EQ(result, 2);
+}
+
+TEST(ae_str_raw_set_value, set_single_character) {
+  ae_char_t buffer[1] = {'A'};
+  ae_char_t value = 'Z';
+
+  ae_str_raw_set_value(buffer, 1, value);
+
+  EXPECT_EQ(buffer[0], 'Z');
+}
+
+TEST(ae_str_raw_set_value, set_multiple_characters) {
+  ae_char_t buffer[3] = {'A', 'B', 'C'};
+  ae_char_t value = 'X';
+
+  ae_str_raw_set_value(buffer, 3, value);
+
+  EXPECT_EQ(buffer[0], 'X');
+  EXPECT_EQ(buffer[1], 'X');
+  EXPECT_EQ(buffer[2], 'X');
+}
+
+TEST(ae_str_raw_set_value, set_zero_length) {
+  ae_char_t buffer[3] = {'A', 'B', 'C'};
+  ae_char_t value = 'X';
+
+  ae_str_raw_set_value(buffer, 0, value);
+
+  EXPECT_EQ(buffer[0], 'A');
+  EXPECT_EQ(buffer[1], 'B');
+  EXPECT_EQ(buffer[2], 'C');
+}
+
+TEST(ae_str_raw_set_value, set_different_values) {
+  ae_char_t buffer[2] = {'A', 'B'};
+
+  ae_str_raw_set_value(buffer, 2, '1');
+  EXPECT_EQ(buffer[0], '1');
+  EXPECT_EQ(buffer[1], '1');
+
+  ae_str_raw_set_value(buffer, 2, '@');
+  EXPECT_EQ(buffer[0], '@');
+  EXPECT_EQ(buffer[1], '@');
+}
+
+TEST(ae_str_raw_set_value, set_special_characters) {
+  ae_char_t buffer[2] = {'A', 'B'};
+  ae_char_t value = '\n';
+
+  ae_str_raw_set_value(buffer, 2, value);
+
+  EXPECT_EQ(buffer[0], '\n');
+  EXPECT_EQ(buffer[1], '\n');
+}
+
+TEST(ae_str_raw_copy, copy_equal_length) {
+  ae_char_t src[] = {'A', 'B', 'C'};
+  ae_char_t dst[3] = {0};
+
+  ae_char_t * result = ae_str_raw_copy(dst, 3, src, 3);
+
+  EXPECT_EQ(dst[0], 'A');
+  EXPECT_EQ(dst[1], 'B');
+  EXPECT_EQ(dst[2], 'C');
+  EXPECT_EQ(result, dst + 3);
+}
+
+TEST(ae_str_raw_copy, copy_shorter_source) {
+  ae_char_t src[] = {'X', 'Y'};
+  ae_char_t dst[3] = {'A', 'B', 'C'};
+
+  ae_char_t * result = ae_str_raw_copy(dst, 3, src, 2);
+
+  EXPECT_EQ(dst[0], 'X');
+  EXPECT_EQ(dst[1], 'Y');
+  EXPECT_EQ(dst[2], 'C');  // Последний символ не должен измениться
+  EXPECT_EQ(result, dst + 2);
+}
+
+TEST(ae_str_raw_copy, copy_longer_source) {
+  ae_char_t src[] = {'P', 'Q', 'R', 'S'};
+  ae_char_t dst[2] = {0};
+
+  ae_char_t * result = ae_str_raw_copy(dst, 2, src, 4);
+
+  EXPECT_EQ(dst[0], 'P');
+  EXPECT_EQ(dst[1], 'Q');
+  EXPECT_EQ(result, dst + 2);
+}
+
+TEST(ae_str_raw_copy, copy_zero_length) {
+  ae_char_t src[] = {'A', 'B'};
+  ae_char_t dst[2] = {'X', 'Y'};
+
+  ae_char_t * result = ae_str_raw_copy(dst, 0, src, 2);
+
+  EXPECT_EQ(dst[0], 'X');
+  EXPECT_EQ(dst[1], 'Y');
+  EXPECT_EQ(result, dst);
+}
+
+TEST(ae_str_raw_copy, copy_empty_source) {
+  ae_char_t src[] = {0};
+  ae_char_t dst[2] = {'A', 'B'};
+
+  ae_char_t * result = ae_str_raw_copy(dst, 2, src, 0);
+
+  EXPECT_EQ(dst[0], 'A');
+  EXPECT_EQ(dst[1], 'B');
+  EXPECT_EQ(result, dst);
+}
+
+TEST(ae_str_raw_copy, copy_special_characters) {
+  ae_char_t src[] = {'\n', '\t', '\0'};
+  ae_char_t dst[3] = {0};
+
+  ae_char_t * result = ae_str_raw_copy(dst, 3, src, 3);
+
+  EXPECT_EQ(dst[0], '\n');
+  EXPECT_EQ(dst[1], '\t');
+  EXPECT_EQ(dst[2], '\0');
+  EXPECT_EQ(result, dst + 3);
 }
--- src/wstr_raw.cpp
+++ src/wstr_raw.cpp
@@ -5,7 +5,7 @@ TEST(ae_wstr_raw_find_char, find_existing_char) {
   const ae_wchar_t str[] = L"Hello";
   ae_usize_t len = 5;
   ae_wchar_t value = L'l';
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, &str[2]);
 }
 
@@ -13,7 +13,7 @@ TEST(ae_wstr_raw_find_char, find_non_existing_char) {
   const ae_wchar_t str[] = L"Hello";
   ae_usize_t len = 5;
   ae_wchar_t value = L'z';
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, nullptr);
 }
 
@@ -21,7 +21,7 @@ TEST(ae_wstr_raw_find_char, empty_string) {
   const ae_wchar_t str[] = L"";
   ae_usize_t len = 0;
   ae_wchar_t value = L'a';
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, nullptr);
 }
 
@@ -29,7 +29,7 @@ TEST(ae_wstr_raw_find_char, zero_length) {
   const ae_wchar_t str[] = L"Hello";
   ae_usize_t len = 0;
   ae_wchar_t value = L'H';
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, nullptr);
 }
 
@@ -37,7 +37,7 @@ TEST(ae_wstr_raw_find_char, repeated_chars) {
   const ae_wchar_t str[] = L"aaaba";
   ae_usize_t len = 5;
   ae_wchar_t value = L'a';
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, &str[0]);
 }
 
@@ -45,7 +45,7 @@ TEST(ae_wstr_raw_find_char, case_sensitive) {
   const ae_wchar_t str[] = L"Hello";
   ae_usize_t len = 5;
   ae_wchar_t value = L'h';
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, nullptr);
 }
 
@@ -53,7 +53,7 @@ TEST(ae_wstr_raw_find_char, russian_chars) {
   const ae_wchar_t str[] = L"\u041F\u0440\u0438\u0432\u0435\u0442";  // Привет
   ae_usize_t len = 6;
   ae_wchar_t value = L'\u0432';  // в
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, &str[3]);
 }
 
@@ -61,7 +61,7 @@ TEST(ae_wstr_raw_find_char, japanese_chars) {
   const ae_wchar_t str[] = L"\u3053\u3093\u306B\u3061\u306F";  // こんにちは
   ae_usize_t len = 5;
   ae_wchar_t value = L'\u306B';  // に
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, &str[2]);
 }
 
@@ -69,7 +69,7 @@ TEST(ae_wstr_raw_find_char, arabic_chars) {
   const ae_wchar_t str[] = L"\u0645\u0631\u062D\u0628\u064B\u0627";  // مرحبًا
   ae_usize_t len = 6;
   ae_wchar_t value = L'\u0631';  // ر
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, &str[1]);
 }
 
@@ -77,7 +77,7 @@ TEST(ae_wstr_raw_find_char, chinese_chars) {
   const ae_wchar_t str[] = L"\u4F60\u597D";  // 你好
   ae_usize_t len = 2;  // Исправлено: длина строки 2, а не 3
   ae_wchar_t value = L'\u4F60';  // 你
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, &str[0]);
 }
 
@@ -85,7 +85,7 @@ TEST(ae_wstr_raw_find_char, korean_chars) {
   const ae_wchar_t str[] = L"\uC548\uB155\uD558\uC138\uC694";  // 안녕하세요
   ae_usize_t len = 5;
   ae_wchar_t value = L'\uD558';  // 하
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, &str[2]);
 }
 
@@ -93,7 +93,7 @@ TEST(ae_wstr_raw_find_char, beyond_length) {
   const ae_wchar_t str[] = L"Hello\0World";
   ae_usize_t len = 5;
   ae_wchar_t value = L'W';
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, nullptr);
 }
 
@@ -101,7 +101,7 @@ TEST(ae_wstr_raw_find_char, find_null_char) {
   const ae_wchar_t str[] = L"Hello\0";
   ae_usize_t len = 6;
   ae_wchar_t value = L'\0';
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, &str[5]);
 }
 
@@ -109,30 +109,30 @@ TEST(ae_wstr_raw_find_char, null_string) {
   const ae_wchar_t *str = nullptr;
   ae_usize_t len = 0;
   ae_wchar_t value = L'a';
-  const ae_wstr_raw_t result = ae_wstr_raw_find_char(str, len, value);
+  const ae_wchar_t * result = ae_wstr_raw_find_char(str, len, value);
   EXPECT_EQ(result, nullptr);
 }
 
 TEST(ae_wstr_raw_find_of_null_terminator, empty_string) {
   const ae_wchar_t str[] = L"";
-  const ae_wstr_raw_t result = ae_wstr_raw_find_of_null_terminator(str);
+  const ae_wchar_t * result = ae_wstr_raw_find_of_null_terminator(str);
   EXPECT_EQ(result, str);
 }
 
 TEST(ae_wstr_raw_find_of_null_terminator, single_char_string) {
   const ae_wchar_t str[] = L"a";
-  const ae_wstr_raw_t result = ae_wstr_raw_find_of_null_terminator(str);
+  const ae_wchar_t * result = ae_wstr_raw_find_of_null_terminator(str);
   EXPECT_EQ(result, str + 1);
 }
 
 TEST(ae_wstr_raw_find_of_null_terminator, multi_char_string) {
   const ae_wchar_t str[] = L"hello";
-  const ae_wstr_raw_t result = ae_wstr_raw_find_of_null_terminator(str);
+  const ae_wchar_t * result = ae_wstr_raw_find_of_null_terminator(str);
   EXPECT_EQ(result, str + 5);
 }
 
 TEST(ae_wstr_raw_find_of_null_terminator, null_pointer) {
-  const ae_wstr_raw_t result = ae_wstr_raw_find_of_null_terminator(nullptr);
+  const ae_wchar_t * result = ae_wstr_raw_find_of_null_terminator(nullptr);
   EXPECT_EQ(result, nullptr);
 }
 
@@ -152,4 +152,106 @@ TEST(ae_wstr_raw_len, multi_char_string) {
   const ae_wchar_t str[] = L"hello";
   ae_usize_t len = ae_wstr_raw_len(str);
   EXPECT_EQ(len, 5);
+}
+
+TEST(ae_wstr_raw_set_value, set_single_character) {
+  ae_wchar_t buffer[1] = {L'A'};
+  ae_wchar_t value = L'Z';
+
+  ae_wstr_raw_set_value(buffer, 1, value);
+
+  EXPECT_EQ(buffer[0], L'Z');
+}
+
+TEST(ae_wstr_raw_set_value, set_multiple_characters) {
+  ae_wchar_t buffer[3] = {L'A', L'B', L'C'};
+  ae_wchar_t value = L'X';
+
+  ae_wstr_raw_set_value(buffer, 3, value);
+
+  EXPECT_EQ(buffer[0], L'X');
+  EXPECT_EQ(buffer[1], L'X');
+  EXPECT_EQ(buffer[2], L'X');
+}
+
+TEST(ae_wstr_raw_set_value, set_zero_length) {
+  ae_wchar_t buffer[3] = {L'A', L'B', L'C'};
+  ae_wchar_t value = L'X';
+
+  ae_wstr_raw_set_value(buffer, 0, value);
+
+  // Проверяем, что ничего не изменилось
+  EXPECT_EQ(buffer[0], L'A');
+  EXPECT_EQ(buffer[1], L'B');
+  EXPECT_EQ(buffer[2], L'C');
+}
+
+TEST(ae_wstr_raw_set_value, set_different_values) {
+  ae_wchar_t buffer[2] = {L'A', L'B'};
+
+  // Тест с разными значениями
+  ae_wstr_raw_set_value(buffer, 2, L'1');
+  EXPECT_EQ(buffer[0], L'1');
+  EXPECT_EQ(buffer[1], L'1');
+
+  ae_wstr_raw_set_value(buffer, 2, L'@');
+  EXPECT_EQ(buffer[0], L'@');
+  EXPECT_EQ(buffer[1], L'@');
+}
+
+TEST(ae_wstr_raw_copy, copy_equal_length) {
+  ae_wchar_t src[] = {L'A', L'B', L'C'};
+  ae_wchar_t dst[3] = {0};
+
+  ae_wchar_t * result = ae_wstr_raw_copy(dst, 3, src, 3);
+
+  EXPECT_EQ(dst[0], L'A');
+  EXPECT_EQ(dst[1], L'B');
+  EXPECT_EQ(dst[2], L'C');
+  EXPECT_EQ(result, dst + 3);
+}
+
+TEST(ae_wstr_raw_copy, copy_shorter_source) {
+  ae_wchar_t src[] = {L'X', L'Y'};
+  ae_wchar_t dst[3] = {L'A', L'B', L'C'};
+
+  ae_wchar_t * result = ae_wstr_raw_copy(dst, 3, src, 2);
+
+  EXPECT_EQ(dst[0], L'X');
+  EXPECT_EQ(dst[1], L'Y');
+  EXPECT_EQ(dst[2], L'C');  // Последний символ не должен измениться
+  EXPECT_EQ(result, dst + 2);
+}
+
+TEST(ae_wstr_raw_copy, copy_longer_source) {
+  ae_wchar_t src[] = {L'P', L'Q', L'R', L'S'};
+  ae_wchar_t dst[2] = {0};
+
+  ae_wchar_t * result = ae_wstr_raw_copy(dst, 2, src, 4);
+
+  EXPECT_EQ(dst[0], L'P');
+  EXPECT_EQ(dst[1], L'Q');
+  EXPECT_EQ(result, dst + 2);
+}
+
+TEST(ae_wstr_raw_copy, copy_zero_length) {
+  ae_wchar_t src[] = {L'A', L'B'};
+  ae_wchar_t dst[2] = {L'X', L'Y'};
+
+  ae_wchar_t * result = ae_wstr_raw_copy(dst, 0, src, 2);
+
+  EXPECT_EQ(dst[0], L'X');
+  EXPECT_EQ(dst[1], L'Y');
+  EXPECT_EQ(result, dst);
+}
+
+TEST(ae_wstr_raw_copy, copy_empty_source) {
+  ae_wchar_t src[] = {0};
+  ae_wchar_t dst[2] = {L'A', L'B'};
+
+  ae_wchar_t * result = ae_wstr_raw_copy(dst, 2, src, 0);
+
+  EXPECT_EQ(dst[0], L'A');
+  EXPECT_EQ(dst[1], L'B');
+  EXPECT_EQ(result, dst);
 }
